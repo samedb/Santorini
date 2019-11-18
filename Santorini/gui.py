c993e2a7d20e9ba1904ba6b33591beab99783ca9
samedb <41544886+samedb@users.noreply.github.com>
2019-11-18 15:08:01 +0100

    prebaceno ovo sto imam na github

---
+++ Santorini/gui.py
@@ -0,0 +1,175 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from igra import Tabla
+
+LARGE_FONT= ("Verdana bold", 24)
+
+
+class Application(Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        Tk.__init__(self, *args, **kwargs)
+
+        Tk.title(self, "Santorini")
+        Tk.geometry(self, "800x600")
+        Tk.resizable(self, 0, 0)
+        Tk.option_add(self, "*Font", "Arial 16 bold")
+
+        self.container = Frame(self)
+        self.container.pack(side="top", fill='both' , expand = 1)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        self.show_frame(PocetniFrame)
+
+
+    def show_frame(self, cont, *args, **kwarg):
+        frame = None
+        frame = cont(self.container, self, *args, **kwarg)
+        frame.grid(row=0, column=0, sticky="nsew") 
+        frame.tkraise()
+
+
+class PocetniFrame(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self,parent)
+        label = Label(self, text="Santorini\n\n", font=LARGE_FONT)
+        label.pack(pady=20,padx=10)
+
+        if self.ima_nedovrsen_mec():
+            Button(self, text="Nastavi igru", command=lambda: controller.show_frame(IgraFrame, nastavi = "path to file"), width = 25).pack(pady = 5)
+
+        Button(self, text="Nova igra",    command=lambda: controller.show_frame(OdabirTipaIgreFrame), width = 25).pack(pady = 5)
+        Button(self, text="Ucitaj igru",  command=lambda: controller.show_frame(UcitajIgruFrame),     width = 25).pack(pady = 5)
+        Button(self, text="Podesavanja",  command=lambda: controller.show_frame(PodesavanjaFrame),    width = 25).pack(pady = 5)
+        Button(self, text="Pravila",      command=lambda: controller.show_frame(PravilaFrame),        width = 25).pack(pady = 5)
+        Button(self, text="Izlaz",        command=sys.exit,                                           width = 25).pack(pady = 5)
+
+    def ima_nedovrsen_mec(self):
+        return True
+
+
+class OdabirTipaIgreFrame(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        Label(self, text="Odabir tipa i tezine igre", font=LARGE_FONT).grid(row = 0, columnspan = 2, sticky = "n", pady = 30)
+
+        Label(self, text = "Plavi igrac").grid(row = 1, column = 0)
+        cb1 = ttk.Combobox(self, values = ["Osoba", "AI easy", "AI medium", "AI hard"], width = 25)
+        cb1.grid(row = 2, column = 0, padx = 40, pady = 20)
+        cb1.current(0)
+
+        Label(self, text = "Crveni igrac").grid(row = 1, column = 1)
+        cb2 = ttk.Combobox(self, values = ["Osoba", "AI easy", "AI medium", "AI hard"], width = 25)
+        cb2.grid(row = 2, column = 1, padx = 40, pady = 20)
+        cb2.current(0)
+
+        #vs ili protiv izmedju
+        Label(self, text = "vs").grid(row = 2, columnspan = 2)
+
+        objasnjenje_tezina = "Opis raznih nivoa vestacke inteligencije \nLupam \n• AI easy - minimax algoritam sa dubinom 5 \n• AI medium - minimax algoritam sa dubinom 10 uz alfa-beta odsecanje \n• AI hard - minimax algoritam sa dubinom 15 uz alfa-beta odsecanje"
+            
+        Label(self, text=objasnjenje_tezina, justify = LEFT, font = "Arial 16").grid(row = 3, columnspan = 2, pady = 100)
+
+        Button(self, text="Nazad na pocetnu stranu", command=lambda: controller.show_frame(PocetniFrame), width = 20).grid(row = 4, column = 0)
+        
+        
+        Button(self, text="Pokreni igru", command=lambda: controller.show_frame(IgraFrame, igrac1 = cb1.get(), igrac2 = cb2.get()), width = 20).grid(row = 4, column = 1)
+
+
+class IgraFrame(Frame):
+    x_offset = 150
+    y_offset = 50
+
+    def __init__(self, parent, controller, *args, **kwarg):
+        Frame.__init__(self, parent)
+        self.controller = controller
+   #     nastaviMec = kwarg.get("nastavi", None) != None
+   #     if nastaviMec:
+   #         Label(self, text="Igra frame!!!", font=LARGE_FONT).grid(row = 0, column = 0, columnspan = 3)
+   #         Label(self, text=f"nastavlja se neka igra: {kwarg.get('nastavi')}").grid(row = 0, column = 3)
+   #     else:
+   #         igrac1 = kwarg.get("igrac1", None)
+   #         igrac2 = kwarg.get("igrac2", None)
+   #         Label(self, text="Igra frame!!!", font=LARGE_FONT).grid(row = 0, column = 0)
+   #         Label(self, text=f"Ovde treba da bude igra\n {igrac1} vs {igrac2}").grid(row = 0, column = 3)
+
+        Tabla(self).place(x = 150, y = 0)
+        Button(self, text="Nazad na pocetnu stranu", command=self.povratak_na_pocetnu).pack(side = BOTTOM, pady = 3)
+
+    def povratak_na_pocetnu(self):
+        odg = messagebox.askquestion("Nazad na pocetnu stranicu?", "Da li ste sigurni da zelite da prekinete igru i da se vratite na pocetnu stranicu?")
+        if odg == "yes":
+            self.controller.show_frame(PocetniFrame)
+
+
+class UcitajIgruFrame(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        Label(self, text="Ucitavanje igre", font=LARGE_FONT).pack()
+        Label(self, text="Ovde treba da se pojavi lista svih save game-ova\n koji mogu da se ucitaju,\n da se nastavi sa igranjem ili da se pusti replay").pack(pady = 30)
+        Button(self, text="Nazad na pocetnu stranu", command=lambda: controller.show_frame(PocetniFrame)).pack()
+
+
+class PodesavanjaFrame(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        Label(self, text="Podesavanja igre", font=LARGE_FONT).pack(pady = 20)
+        Button(self, text="Grafika", command = self.ne_radi_nista, width = 25).pack(pady = 5)
+        Button(self, text="Audio"  , command = self.ne_radi_nista, width = 25).pack(pady = 5)
+        Button(self, text="Nazad na pocetnu stranu", command=lambda: controller.show_frame(PocetniFrame) , width = 25).pack(pady = 5)
+
+    def ne_radi_nista(self):
+        messagebox.showinfo("XD", "Nema ovde nista :D")
+
+
+class PravilaFrame(Frame):
+
+    pravila = """
+        •  Igra se igra na tabli 5x5, koja se inicijalno sastoji iz 25 praznih polja. Igru igraju 
+        dva igrača, koji povlače poteze naizmenično. Na početku igre, prvi, a zatim i drugi igrač
+        postavljaju svoje figure na bilo koje od slobodnih polja. Nakon postavljanja figura, igrači
+        naizmenično igraju povlačeći poteze.
+
+        •  Potez igrača se sastoji iz pomeranja figure i gradnje. Najpre se jedna odabrana figura
+        pomera na neko od slobodnih susednih polja (uključujući dijagonalne susede, pri čemu
+        odredišno polje mora biti najviše jednu pločicu iznad izvorišnog polja. Ukoliko igrač ne
+        može da pomeri nijednu od svojih figura prema ovim pravilima, gubi igru.
+
+        •  Drugi deo poteza je gradnja. Ova faza podrazumeva dodavanje pločice na jedno od
+        slobodnih polja susednih odredišnom polju pomerene figure (uključujući dijagonalne
+        susede). Pločica koja se dodaje na polje nivoa 3 je kružna kupola. Polja nivoa 4 (3 pločice
+        i kružna kupola na vrhu) su „blokirana“ u nastavku igra. Nije dozvoljeno postavljanje figura
+        na takva polja, niti dalja izgradnja nad tim poljima.
+
+        •  Igra se može završiti na dva načina:
+            •   kada igrač koji je na potezu pomeri jednu od figura na polje nivoa 3 (izgrađene tri
+                pločice na tom polju) – pobednik je igrač koji je pomerio svoju figuru na polje nivoa 3;
+            •   kada igrač koji je na potezu ne može da odigra potez prema pravilima igre –
+                pobednik je igrač koji nije na potezu."""
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        Label(self, text="Pravila igre Santorini", font=LARGE_FONT).pack(pady = 20)
+        Label(self, text = self.pravila, font = "Arial 13", justify = LEFT).pack(pady = 10)
+        Button(self, text="Nazad na pocetnu stranu", command=lambda: controller.show_frame(PocetniFrame)).pack(side = BOTTOM, pady = 10)
+
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
+
+    #todo celava latinica
+    #todo da prosledjujem parametre izmedju frame-ova  - uradnjeno
+    #todo da izdvojim ove klase u posebne fajlove
+
+    #combobox da bude readonly\
+
+
---
+++ Santorini/igra.py
@@ -0,0 +1,259 @@
+from tkinter import *
+from enum import Enum
+from tkinter import messagebox
+
+
+class Igrac(Enum):
+    PLAVI = 0
+    CRVENI = 1
+
+
+class GameState(Enum):
+    POSTAVLJANJE_FIGURA = 0
+    SELEKTOVANJE_FIGURE = 1
+    POMERANJE_FIGURE = 2
+    GRADNJA = 3 
+
+class Polje():
+    broj_spratova = 0
+    igrac = None
+
+    def uradiNesto(self, event):
+        print("Radim nesto")
+
+
+class Stanje():
+
+    def __init__(self):
+        self.matrica = []
+        for i in range (0, 5):
+            red = []
+            for j in range(0, 5):
+                polje = Polje()
+                #polje.broj_spratova = i
+                #polje.igrac = j
+                red.append(polje)
+            self.matrica.append(red)
+
+    def izvrsi_potez(self, potez):
+        # todo 
+        print("Izvrsava se potez")
+
+
+class Tabla(Canvas):
+
+    def __init__(self, parent):
+        Canvas.__init__(self, parent)
+        self.config(width = 500, height = 550) 
+        self.bind("<Button-1>", self.mouse_click)
+        #inicijalizacije matrice
+        self.stanje = Stanje()
+        self.na_potezu = Igrac.PLAVI # plavi igrac je prvi na potezu
+        self.game_state = GameState.POSTAVLJANJE_FIGURA # u pocetku svi igraci postavljaju svoje figure na tablu
+        self.broj_figura = 0 # treba mi za prvu fazu gde se postavljaju figure
+        self.kraj_meca = False
+        self.sastavi_poruku()
+        self.dozvoljena_polja = []
+        self.selektovana_figura = None
+        self.crtaj(self.stanje)
+    
+    def crtaj(self, stanje):
+        self.delete("all")
+        for i in range (0, 5):
+            for j in range(0, 5):
+                #izracunaj koordinate tog polja
+                x1 = i * 100
+                y1 = j * 100 + 50
+                #nacrtaj pozadinu
+                if self.selektovana_figura == (i, j): # ako je to polje na kojem se nalazi selektovana figura onda oboji zutom bojom
+                    boja = "yellow"
+                elif self.dozvoljena_polja.__contains__((i, j)): #ako je to polje dozvoljeno u sledecem potezu onda zelenom bojom
+                    boja = "green"
+                else:
+                    boja = "grey" # ako nije nista od toga onda siva boja
+                self.create_rectangle(x1 + 2, y1 + 2, x1 + 98, y1 + 98, fill = boja)
+                #nacrtaj spratove
+                stepen = stanje.matrica[i][j].broj_spratova
+                if stepen >= 1:
+                    self.create_rectangle(x1 + 8, y1 + 8, x1 + 92, y1 + 92, width = 3)
+                if stepen >= 2:
+                    self.create_rectangle(x1 + 15, y1 + 15, x1 + 85, y1 + 85, width = 3)
+                if stepen >= 3:
+                    self.create_rectangle(x1 + 25, y1 + 25, x1 + 75, y1 + 75, width = 3)
+                if stepen >= 4:
+                    self.create_oval(x1 + 30, y1 + 30, x1 + 70, y1 + 70, fill = "black")
+                #nacrtaj igraca
+                if stanje.matrica[i][j].igrac == Igrac.PLAVI: # 0 znaci plavi igrac
+                    self.create_oval(x1 + 30, y1 + 30, x1 + 70, y1 + 70, fill = "blue")
+                elif stanje.matrica[i][j].igrac == Igrac.CRVENI: # 1 znaci crveni igrac
+                    self.create_oval(x1 + 30, y1 + 30, x1 + 70, y1 + 70, fill = "red")
+
+        self.create_text(250, 25, text = self.poruka, font = "Airal 12")
+
+    def mouse_click(self, e):
+        print(e.x, e.y) # koordinate piksela na koji je klikuo mis u ovom canvasu
+        #ako je kliknuo vam table ili je kraj meca
+        if e.y < 50 or self.kraj_meca:
+            return
+        # preracunavanje tih kooridnata u indekse polja tabele
+        x = int(e.x / 100)
+        y = int((e.y - 50) / 100)
+        print(f"Polje koje je pritisnuto je: {x}, {y}")
+        #self.stanje.matrica[x][y].broj_spratova += 1
+
+        try:
+            if self.game_state == GameState.POSTAVLJANJE_FIGURA:
+                self.postavi_figuru(x, y) 
+                self.broj_figura += 1
+                if self.broj_figura == 2:
+                    self.zameni_igraca()
+                elif self.broj_figura == 4:
+                    self.game_state = GameState.SELEKTOVANJE_FIGURE
+                    self.zameni_igraca()
+                    print("Sad pocinje prava igra")
+
+            elif self.game_state == GameState.SELEKTOVANJE_FIGURE:
+                self.selektuj_figuru(x, y)
+                self.game_state = GameState.POMERANJE_FIGURE
+                self.sastavi_poruku()
+                self.pronadji_dozvoljena_polja()
+
+            elif self.game_state == GameState.POMERANJE_FIGURE:
+                #ako klikne na istu figuru ili na svoju drugu figuru
+                if self.stanje.matrica[x][y].igrac == self.na_potezu:
+                    self.selektuj_figuru(x, y)
+                else:
+                    x1, y1 = self.selektovana_figura
+                    self.pomeri_figuru(x1, y1, x, y)
+                    self.game_state = GameState.GRADNJA
+                
+                self.pronadji_dozvoljena_polja()
+                self.sastavi_poruku()
+            elif self.game_state == GameState.GRADNJA:
+                self.gradi(x, y)
+                self.game_state = GameState.SELEKTOVANJE_FIGURE
+                self.sastavi_poruku()
+                self.zameni_igraca()
+                self.pronadji_dozvoljena_polja()
+
+                #proveri da li je kraj meca, tj da li ima mogucih poteza za igraca na potezu
+                if not self.ima_mogucih_poteza():
+                    if self.na_potezu == Igrac.CRVENI:
+                        pobednik = Igrac.PLAVI
+                    else:
+                        pobednik = Igrac.CRVENI
+                    self.pobeda(f"Pobedio je {pobednik} jer {self.na_potezu} nema mogucih poteza!")
+
+                
+        except Exception as error:
+            print(error)
+
+
+        #crtaj ide na kraj ove funkcije
+        self.crtaj(self.stanje)
+
+    def selektuj_figuru(self, x, y):
+        if self.stanje.matrica[x][y].igrac == self.na_potezu:
+            self.selektovana_figura = (x, y)
+        else:
+            raise Exception("Morate da selektujete neku vasu figuru")
+
+
+
+    def postavi_figuru(self, x, y):
+        if self.stanje.matrica[x][y].igrac == None:
+            self.stanje.matrica[x][y].igrac = self.na_potezu
+        else:
+            raise Exception("Polje je vec zauzeto")
+
+    def pomeri_figuru(self, x1, y1, x2, y2):
+        if self.dozvoljena_polja.__contains__((x2, y2)):
+            figura = self.stanje.matrica[x1][y1].igrac
+            self.stanje.matrica[x1][y1].igrac = None
+            self.stanje.matrica[x2][y2].igrac = figura
+            self.selektovana_figura = (x2, y2) # slektovana figura/polje je polje gde se sada nalazi figura, jer gradimo u susednim poljima
+
+            #proveri da li je figura postavljena na polje sa brojem spratova 3, ako jeste onda je pobeda
+            if self.stanje.matrica[x2][y2].broj_spratova == 3:
+                self.crtaj(self.stanje)
+                self.pobeda(f"Pobedio je {self.na_potezu} jer je zauzeo polje sa nivoom 3!")
+        else:
+            raise Exception("Nedozvoljen potez")
+
+    def gradi(self, x, y):
+        if self.dozvoljena_polja.__contains__((x, y)):
+            self.stanje.matrica[x][y].broj_spratova += 1
+        else:
+            raise Exception("Ne mozete da gradite na ovom polju")
+
+    def zameni_igraca(self):
+        if self.na_potezu == Igrac.PLAVI:
+            self.na_potezu = Igrac.CRVENI
+        else:
+            self.na_potezu = Igrac.PLAVI
+
+        self.sastavi_poruku()
+        self.pronadji_dozvoljena_polja()
+        self.selektovana_figura = None
+
+
+        '''
+            if trenutni igrac == AI
+                sledeci_potez = AI.potez(trenutno_stanje)
+                izvrsi_potez(sledeci_potez) # ova funkcija crta redom sve korake, vise puta poziva funkciju crtaj da bi pokazala kako se sleketuje figura
+                pomera figura i gradi, sa nekim delay-om izmedju koraka
+                zameni_igraca()
+        '''
+
+    def pobeda(self, poruka):
+        self.kraj_meca = True
+        messagebox.showinfo("Pobeda", poruka)
+        self.crtaj(self.stanje)
+
+    def ima_mogucih_poteza(self):
+        for x in range(0, 5):
+            for y in range(0, 5):
+                if self.stanje.matrica[x][y].igrac == self.na_potezu:
+                    # prodji kroz sva susedna polja i proveri da li moze na njih da predje, ako ne moze onda je izgubio 
+                    for i in range(x - 1, x + 2):
+                        for j in range(y - 1, y + 2):
+                            if i >= 0 and i <= 4 and j >= 0 and j <= 4 and self.stanje.matrica[i][j].igrac == None and self.stanje.matrica[i][j].broj_spratova <= self.stanje.matrica[x][y].broj_spratova + 1:
+                                print("Prvo moguce polje je ", i , j, " za " , x, y)
+                                return True
+        return False
+
+    def sastavi_poruku(self):
+        if not self.kraj_meca:
+            self.poruka = f"Na potezu je {self.na_potezu} \na gamestate je {self.game_state}"
+
+    def pronadji_dozvoljena_polja(self):
+        self.dozvoljena_polja.clear()
+
+        if self.game_state == GameState.SELEKTOVANJE_FIGURE:
+            for i in range(0, 5):
+                for j in range(0, 5):
+                    if self.stanje.matrica[i][j].igrac == self.na_potezu:
+                        self.dozvoljena_polja.append((i, j))
+        elif self.game_state == GameState.POMERANJE_FIGURE:
+            x, y = self.selektovana_figura
+            # prodji kroz sva polja u neposrednoj blizini selektovane figure
+            for i in range(x - 1, x + 2):
+                for j in range(y - 1, y + 2):
+                    if i >= 0 and i <= 4 and j >= 0 and j <= 4 and self.stanje.matrica[i][j].igrac == None and self.stanje.matrica[i][j].broj_spratova <= self.stanje.matrica[x][y].broj_spratova + 1 and self.stanje.matrica[i][j].broj_spratova < 4:   #ovde nisam siguran da li moze figura da se spusit sa 2. na 0. sprat
+                        self.dozvoljena_polja.append((i, j))
+        elif self.game_state == GameState.GRADNJA:
+            x, y = self.selektovana_figura
+            # prodji kroz sva polja u neposrednoj blizini selektovane figure
+            for i in range(x - 1, x + 2):
+                for j in range(y - 1, y + 2):
+                    if i >= 0 and i <= 4 and j >= 0 and j <= 4 and self.stanje.matrica[i][j].igrac == None and self.stanje.matrica[i][j].broj_spratova < 4:   #ovde nisam siguran da li moze figura da se spusit sa 2. na 0. sprat
+                        self.dozvoljena_polja.append((i, j))
+        
+        
+        
+        #todo da sredim ova govna, funkcije da prepravim, i da izbacim suvisne i redudnantne delove
+
+        # da pogedam kako je uradio onaj lik sa githuba i da mu ukradem po koju foru
+    
+
+    
